6e112edcfb2f020296e6995964857be223929cfd
Enzo Souza <enzo.fmcs@gmail.com>
2025-04-16 10:43:50 -0300

    Adding the template archive

---
+++ template.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false);
+#define fo(i,n) for(i=0;i<n;i++)
+#define si(x)	cin >> x
+#define pi(x)   cout << x
+#define pl(x)   cout << x << "\n"
+#define all(x)  x.begin(), x.end()
+#define PI 3.1415926535897932384626
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl; 
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+int mpow(int base, int exp) {
+    base %= mod;
+    int result = 1;
+    while (exp > 0) {
+      if (exp & 1) result = ((ll)result * base) % mod;
+      base = ((ll)base * base) % mod;
+      exp >>= 1;
+    }
+    return result;
+  }
+
+
+int main(){
+    fastio;
+    pi("This is the Template!");
+
+
+    return 0;
+}
